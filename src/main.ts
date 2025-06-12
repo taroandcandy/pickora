aa3a31ddd186f641263ecdc9620795a99de0bd3d
XuHuan <taroandcandy@gmail.com>
2025-06-12 21:25:45 +0800

    nolmiliztion

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
-const a = () => {
-    console.log('今天好累')
-    console.log('完全不想学习')
+const a = () => 
+  {
+  console.log('今天好累')
+  console.log('完全不想学习')
 }
 a()
